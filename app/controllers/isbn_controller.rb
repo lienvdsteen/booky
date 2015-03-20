99560bb8ac6954a9f35991e0c34129abd6d83a14
Andrew Fecheyr <andrew@bedesign.be>
2015-03-20 09:27:15 +0100

    Get title and book cover

--- app/controllers/isbn_controller.rb
+++ app/controllers/isbn_controller.rb
@@ -1,5 +1,10 @@
 class IsbnController < ApplicationController
   def show
     @isbn = params[:isbn]
+    client = Openlibrary::Client.new
+    keys = client.book_by_isbn(@isbn)
+    olid = keys.first.values.first.split('/').last
+    @book = client.book(olid)
   end
+
 end
