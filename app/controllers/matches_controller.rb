1d78f0d4a6f77da1aae396f9905dcf467e15f184
Michael Vanderheeren <michael.vanderheeren@gmail.com>
2015-03-21 13:17:59 +0100

    real matches

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -4,5 +4,14 @@ class MatchesController < ApplicationController
   def index
     @title = 'Matches'
     @page = 'matches'
+
+    client = Openlibrary::Client.new
+
+    @matches = ['0345803485', '0345803485', '0345803485']
+    @matches.map! do | match |
+      keys = client.book_by_isbn(match)
+      olid = keys.first.values.first.split('/').last
+      client.book(olid)
+    end
   end
 end
