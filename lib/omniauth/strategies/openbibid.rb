c5539a52dbd638cba4746bcebc3e51ffeb00f0db
Andrew Fecheyr <andrew@bedesign.be>
2015-03-21 10:42:17 +0100

    wip omniauth

--- lib/omniauth/strategies/openbibid.rb
+++ lib/omniauth/strategies/openbibid.rb
@@ -1,9 +1,39 @@
-require 'omniauth-oauth'
-
 module OmniAuth
   module Strategies
     class Openbibid < OmniAuth::Strategies::OAuth
-      # TODO
+      # Give your strategy a name.
+      option :name, "openbibid"
+
+      # This is where you pass the options you would pass when
+      # initializing your consumer from the OAuth gem.
+      option :client_options, { :site => 'https://mijn.bibliotheek.be/openbibid/rest',
+                               :request_token_path => "/requestToken",
+                               :access_token_path  => "/accessToken",
+                               :authorize_path     => "/auth/authorize"}
+
+      # These are called after authentication has succeeded. If
+      # possible, you should try to set the UID without making
+      # additional calls (if the user id is returned with the token
+      # or as a URI parameter). This may not be possible with all
+      # providers.
+      uid { request.params['userId'] }
+
+      info do
+        {
+          :name => raw_info['name'],
+          :location => raw_info['city']
+        }
+      end
+
+      extra do
+        {
+          'raw_info' => raw_info
+        }
+      end
+
+      def raw_info
+        @raw_info ||= MultiJson.decode(access_token.get("/user/info/#{uid}")).body
+      end
     end
   end
 end
