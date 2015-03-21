82e447f60d240e36fcc1e4b9f0b8edf2cd556bf6
Michael Vanderheeren <michael.vanderheeren@gmail.com>
2015-03-21 13:46:22 +0100

    adding swipe

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -3,3 +3,16 @@ function toggleMatches() {
     $("body").toggleClass("open-matches");
   });
 }
+
+$(function() {
+  $(".swipe-content img").swipe( {
+    //Generic swipe handler for all directions
+    swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+      if (direction === 'left') {
+        $('.btn-not').click();
+      } else if (direction === 'right') {
+        $('.btn-yes').click();
+      }
+    }
+  });
+});
