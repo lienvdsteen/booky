f96d6897cbb798d41d85faaf88748121691acf5b
Andrew Fecheyr <andrew@bedesign.be>
2015-03-21 12:21:24 +0100

    Must be logged in to visit swipe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
+  private
+  helper_method :current_user
+  def current_user
+    User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authenticate_user!
+    current_user || redirect_to('/')
+  end
 end
