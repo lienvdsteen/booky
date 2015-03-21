46dc5594d606d8ea01f20887c8ac471debde3ce9
Andrew Fecheyr <andrew@bedesign.be>
2015-03-21 14:28:16 +0100

    books books

--- app/controllers/swipe_controller.rb
+++ app/controllers/swipe_controller.rb
@@ -1,21 +1,26 @@
 class SwipeController < ApplicationController
   def index
-    @page = 'swipe'
-    @isbn = '0345803485'
-    client = Openlibrary::Client.new
-    keys = client.book_by_isbn(@isbn)
-    olid = keys.first.values.first.split('/').last
-    @book = client.book(olid)
+    redirect_to_next_book
   end
 
   def show
+    @page = 'swipe'
+    @book = Book.find(params[:id])
+    render action: :index
   end
 
   def like
     @user_swiped_rating = UserSwipedRating.create!(isbn: params[:id], user_id: current_user.id, rating: true)
+    redirect_to_next_book
   end
 
   def dislike
     @user_swiped_rating = UserSwipedRating.create!(isbn: params[:id], user_id: current_user.id, rating: false)    
+    redirect_to_next_book
+  end
+
+  private
+  def redirect_to_next_book
+    redirect_to swipe_path(Book.random)
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,13 @@
 class Book < ActiveRecord::Base
+  def self.random
+    Book.find(self.random_id)
+  end
+
+  def self.random_id
+    rand(Book.count)
+  end
+
+  def image_url
+    "http://webservices.bibliotheek.be/index.php?func=cover&ISBN=#{isbn}&coversize=large"
+  end
 end
