e85e20e70758d676e68eda103f8c67f75f5be8e8
Lien Van Den Steen <lienvandensteen@gmail.com>
2015-03-21 12:50:14 +0100

    put code back

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,11 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def create
     @user = User.find_or_create_by_auth_hash(auth_hash)
+    @user.update_attributes(token: auth_hash.extra.access_token, token_secret: auth_hash.extra.access_token.secret)
     session[:user_id] = @user.id
-    # redirect_to '/'
-    render text: auth_hash.inspect
+    redirect_to '/swipe'
   end
 
   protected
