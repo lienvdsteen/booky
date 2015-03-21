0ec7b9e0c2285c627330e7ae8dbd2ab784d3bbbd
Michael Vanderheeren <michael.vanderheeren@gmail.com>
2015-03-21 11:37:26 +0100

    feat(matches): add matches page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   get '/app', to: 'app#index'
+  get '/matches', to: 'matches#index'
   get '/auth/:provider/callback', to: 'sessions#create'
   get 'isbn/:isbn' => 'isbn#show'
 end
