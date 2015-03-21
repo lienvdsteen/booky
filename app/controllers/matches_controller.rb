7118eb73cbabc841f1dd345f745d4b73cc7ae94a
Dennis Verjans <dennisverjans@gmail.com>
2015-03-21 12:13:56 +0100

    css

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -2,5 +2,7 @@ class MatchesController < ApplicationController
   def index
     @title = 'Matches'
     @navbar_class = 'navbar-matches'
+    @body_class = 'body-matches'
+    @html_class = 'html-matches'
   end
 end
